9b562aac4298796ec991bb7dcf194df91765ccb1
lerboi <leroyzzng@gmail.com>
2025-12-15 02:20:18 +0800

    added models and admin config

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import NetflixTitle
 
 # Register your models here.
+
+@admin.register(NetflixTitle)
+class NetflixTitleAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for NetflixTitle model.
+    """
+    list_display = ('show_id', 'title', 'type', 'release_year', 'rating', 'date_added')
+    list_filter = ('type', 'rating', 'release_year')
+    search_fields = ('title', 'director', 'cast', 'description')
+    ordering = ('-date_added', '-release_year')
+    readonly_fields = ('created_at', 'updated_at')
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('show_id', 'type', 'title')
+        }),
+        ('Production Details', {
+            'fields': ('director', 'cast', 'country')
+        }),
+        ('Release Information', {
+            'fields': ('release_year', 'date_added', 'rating', 'duration')
+        }),
+        ('Content Details', {
+            'fields': ('listed_in', 'description')
+        }),
+        ('Metadata', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
